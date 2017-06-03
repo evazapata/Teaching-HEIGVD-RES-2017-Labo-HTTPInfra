3950af5e29c6c53f6c10f011d30b8999ece0041b
Denise Gemesio <denise.gemesio@hotmail.com>
2017-06-04 00:18:43 +0200

    Step 2: done

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,4 +1,47 @@
+// PART A
+
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log("Good morning " + chance.country({ full:true }) + " !");
+//console.log("Good morning " + chance.country({ full:true }) + " !");
+
+
+// PART B
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send( generateCountries() );
+});
+
+app.listen(3000, function() {
+	console.log('Accepting HTTP requests on port 3000');
+});
+
+function generateCountries() {
+	var numberOfCountries = chance.integer({
+		min: 0,
+		max: 10
+	});
+	
+	console.log(numberOfCountries);
+	
+	var countries = [];
+
+	for (var i = 0; i < numberOfCountries; i++) {
+		var population = chance.integer({
+			min: 1000,
+			max: 35000000
+		});
+		var mayor = chance.name();
+
+		countries.push({
+			country : chance.country({ full:true }),
+			population : population,
+			mayor : mayor
+		});
+	};
+	console.log(countries);
+	return countries;
+}
