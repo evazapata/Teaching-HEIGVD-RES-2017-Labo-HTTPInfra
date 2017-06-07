62b34ab2edacd040f87d45354649ce4be2ed84bb
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2017-06-07 18:25:57 +0200

    Next steps in demo

--- demo.sh
+++ demo.sh
@@ -16,36 +16,56 @@ set -u
 # Constants
 ROOT=$(pwd)
 
-IMAGE_NAME=res/express-image
-IMAGE_SOURCE=docker-images/express-image/
+STATIC_IMAGE_NAME=res/apache-php
+STATIC_IMAGE_SOURCE=docker-images/apache-php-image/
+STATIC_SOURCE_PORT=2999
+STATIC_DEST_PORT=80
 
-SOURCE_PORT=80
-DEST_PORT=3000
+DYNAMIC_IMAGE_NAME=res/express-image
+DYNAMIC_IMAGE_SOURCE=docker-images/express-image/
+DYNAMIC_SOURCE_PORT=3000
+DYNAMIC_DEST_PORT=3000
+
+REVERSE_IMAGE_NAME=res/apache-reverse-proxy
+REVERSE_IMAGE_SOURCE=docker-images/apache-reverse-proxy/
+REVERSE_SOURCE_PORT=80
+REVERSE_DEST_PORT=3001
 
 ######################################################
 # Script
+containers=()
+
 echo "This is the demo script for Step 3: Reverse proxy with apache (static configuration)."
 read -p "Press <Enter> to run the demo."
 
 echo "Installing npm modules..."
-cd "$IMAGE_SOURCE/src"
+cd "$DYNAMIC_IMAGE_SOURCE/src"
 npm install --save chance
 npm install --save express
 cd "$ROOT"
 
-echo "Building docker image..."
-sudo docker build --tag "$IMAGE_NAME" "$IMAGE_SOURCE"
+echo "Building docker images..."
+sudo docker build --tag "$STATIC_IMAGE_NAME" "$STATIC_IMAGE_SOURCE"
+sudo docker build --tag "$DYNAMIC_IMAGE_NAME" "$DYNAMIC_IMAGE_SOURCE"
+#sudo docker build --tag "$REVERSE_IMAGE_NAME" "$REVERSE_IMAGE_SOURCE"
 
-echo "Running docker image..."
-containerId=$(sudo docker run --detach --publish $SOURCE_PORT:$DEST_PORT "$IMAGE_NAME")
+echo "Starting $STATIC_IMAGE_NAME image..."
+containers+=$(sudo docker run --detach --publish $STATIC_SOURCE_PORT:$STATIC_DEST_PORT "$STATIC_IMAGE_NAME")
 
-echo "You can now open a browser and access to: http://localhost:$SOURCE_PORT"
+echo "Starting $DYNAMIC_IMAGE_NAME image..."
+containers+=$(sudo docker run --detach --publish $DYNAMIC_SOURCE_PORT:$DYNAMIC_DEST_PORT "$DYNAMIC_IMAGE_NAME")
+
 read -p "Press <Enter> to quit the demo."
 
-echo "Killing container..."
-sudo docker kill "$containerId"
+echo "Killing containers..."
+for container in "${containers[@]}"; do
+    sudo docker kill "${container}"
+done
+
 
 echo "Removing docker image..."
-sudo docker rmi --force "$IMAGE_NAME"
+sudo docker rmi --force "$STATIC_IMAGE_NAME"
+sudo docker rmi --force "$DYNAMIC_IMAGE_NAME"
+sudo docker rmi --force "$REVERSE_IMAGE_NAME"
 
 echo "Demo done !"
