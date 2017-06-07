702ef4f4a77839bd501c1435bd3dd6ac1736b04b
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2017-06-07 17:44:15 +0200

    Demo updated

--- demo.sh
+++ demo.sh
@@ -14,6 +14,9 @@ set -u
 
 ######################################################
 # Constants
+IMAGE_NAME=res/apache-php
+IMAGE_SOURCE=docker-images/apache-php-image/
+
 SOURCE_PORT=80
 DEST_PORT=80
 
@@ -23,10 +26,10 @@ echo "This is the demo script for Step 1: Static HTTP server with apache httpd."
 read -p "Press <Enter> to run the demo."
 
 echo "Building docker image..."
-sudo docker build --tag res/apache-php docker-images/apache-php-image/
+sudo docker build --tag "$IMAGE_NAME" "$IMAGE_SOURCE"
 
 echo "Running docker image..."
-containerId=$(sudo docker run --detach --publish $SOURCE_PORT:$DEST_PORT res/apache-php)
+containerId=$(sudo docker run --detach --publish $SOURCE_PORT:$DEST_PORT "$IMAGE_NAME")
 
 echo "You can now open a browser and access to: http://localhost:$SOURCE_PORT"
 read -p "Press <Enter> to quit the demo."
