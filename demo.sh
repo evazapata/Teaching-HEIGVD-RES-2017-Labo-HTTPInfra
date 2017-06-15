a4b5852ccca04e356f20f6585ebfaf5a1cc7b6e4
root <root@HP-EliteBook>
2017-06-16 00:14:18 +0200

    Demo

--- demo.sh
+++ demo.sh
@@ -37,15 +37,15 @@ echo "Building docker image..."
 docker build --tag "$IMAGE_NAME" "$IMAGE_SOURCE"
 
 echo "Running docker image..."
-containerId=$(docker run --detach --publish $SOURCE_PORT:$DEST_PORT "$IMAGE_NAME")
+docker run --detach --publish $SOURCE_PORT:$DEST_PORT "$IMAGE_NAME"
 
-echo "You can now open a browser and access to: http://localhost:$SOURCE_PORT"
+echo "You can now open a browser and access to: demo.res.ch:$SOURCE_PORT"
 read -p "Press <Enter> to quit the demo."
 
 echo "Killing container..."
-docker kill "$containerId"
+docker kill $(docker ps -a -q)
 
 echo "Removing docker image..."
-docker rmi --force "$IMAGE_NAME"
+docker rm $(docker ps -a -q)
 
 echo "Demo done !"
