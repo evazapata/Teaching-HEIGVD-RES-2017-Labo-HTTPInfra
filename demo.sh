38ccda7da8d2ba6ff8ed32a22b9051f1985f3916
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2017-06-15 23:18:18 +0200

    Demo

--- demo.sh
+++ demo.sh
@@ -18,22 +18,17 @@ ROOT=$(pwd)
 
 STATIC_IMAGE_NAME=res/apache-php
 STATIC_IMAGE_SOURCE=docker-images/apache-php-image/
-STATIC_SOURCE_PORT=2999
-STATIC_DEST_PORT=80
 
 DYNAMIC_IMAGE_NAME=res/express-image
 DYNAMIC_IMAGE_SOURCE=docker-images/express-image/
-DYNAMIC_SOURCE_PORT=3000
-DYNAMIC_DEST_PORT=3000
 
 REVERSE_IMAGE_NAME=res/apache-reverse-proxy
 REVERSE_IMAGE_SOURCE=docker-images/apache-reverse-proxy/
-REVERSE_SOURCE_PORT=80
-REVERSE_DEST_PORT=3001
+REVERSE_SOURCE_PORT=8080
+REVERSE_DEST_PORT=80
 
 ######################################################
 # Script
-containers=()
 
 echo "This is the demo script for Step 3: Reverse proxy with apache (static configuration)."
 read -p "Press <Enter> to run the demo."
@@ -47,27 +42,26 @@ cd "$ROOT"
 echo "Building docker images..."
 docker build --tag "$STATIC_IMAGE_NAME" "$STATIC_IMAGE_SOURCE"
 docker build --tag "$DYNAMIC_IMAGE_NAME" "$DYNAMIC_IMAGE_SOURCE"
-#docker build --tag "$REVERSE_IMAGE_NAME" "$REVERSE_IMAGE_SOURCE"
+docker build --tag "$REVERSE_IMAGE_NAME" "$REVERSE_IMAGE_SOURCE"
 
 echo "Starting $STATIC_IMAGE_NAME image..."
-containers+=$(docker run --detach --publish $STATIC_SOURCE_PORT:$STATIC_DEST_PORT "$STATIC_IMAGE_NAME")
+staticContainerId=$(docker run --detach "$STATIC_IMAGE_NAME")
 
 echo "Starting $DYNAMIC_IMAGE_NAME image..."
-containers+=$(docker run --detach --publish $DYNAMIC_SOURCE_PORT:$DYNAMIC_DEST_PORT "$DYNAMIC_IMAGE_NAME")
+dynamicContainerId=$(docker run --detach "$DYNAMIC_IMAGE_NAME")
 
 echo "Starting $REVERSE_IMAGE_NAME image..."
-#containers+=$(docker run --detach --publish $REVERSE_SOURCE_PORT:$REVERSE_DEST_PORT "$REVERSE_IMAGE_NAME")
+reverseContainerId=$(docker run --detach --publish $REVERSE_SOURCE_PORT:$REVERSE_DEST_PORT "$REVERSE_IMAGE_NAME")
 
-echo "You can now try to access to localhost:$STATIC_SOURCE_PORT, it's the $STATIC_IMAGE_NAME image, shouldn't be accessible."
-echo "You can now try to access to localhost:$DYNAMIC_SOURCE_PORT, it's the $DYNAMIC_IMAGE_NAME image, shouldn't be accessible."
+echo "You can now try to access to localhost, it's the $STATIC_IMAGE_NAME image, shouldn't be accessible."
+echo "You can now try to access to localhost, it's the $DYNAMIC_IMAGE_NAME image, shouldn't be accessible."
 echo "You can now try to access to localhost:$REVERSE_SOURCE_PORT, it's the $REVERSE_IMAGE_NAME image, should be accessible !"
 read -p "Press <Enter> to quit the demo."
 
 echo "Killing containers..."
-for container in "${containers[@]}"; do
-    docker kill "${container}"
-done
-
+docker kill "$staticContainerId"
+docker kill "$dynamicContainerId"
+docker kill "$reverseContainerId"
 
 echo "Removing docker image..."
 docker rmi --force "$STATIC_IMAGE_NAME"
