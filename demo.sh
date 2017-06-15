fbee1bcc9c0eb85519be40231f58be3ed75bc24f
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2017-06-16 00:15:45 +0200

    Demo

--- demo.sh
+++ demo.sh
@@ -31,11 +31,11 @@ docker build --tag "$IMAGE_NAME" "$IMAGE_SOURCE"
 echo "Running docker image..."
 containerId=$(docker run --detach --publish $SOURCE_PORT:$DEST_PORT "$IMAGE_NAME")
 
-echo "You can now open a browser and access to: http://localhost:$SOURCE_PORT"
+echo "You can now open a browser and access to: demo.res.ch:$SOURCE_PORT"
 read -p "Press <Enter> to quit the demo."
 
 echo "Killing container..."
-docker stop $(docker ps -a -q)
+docker kill $(docker ps -a -q)
 
 echo "Removing docker image..."
 docker rm $(docker ps -a -q)
